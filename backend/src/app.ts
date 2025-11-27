ec2f626a2a3eddec0b6a1761d4d185aa8784a0b4
NACXIIX <nicocordano@hotmail.com>
2025-11-27 17:36:55 -0300

    feat(backend): implementada la relacion de modelos y logica de expiracion

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,17 @@
 import Server from './config/appConfig'
 import { connectDB, createDatabaseIfNotExists } from './config/dbConfig'
+import { modelRelations } from './models/ModelsRelations'
+import ReservaService from './services/ReservaService'
 
 async function app(): Promise<void> {
     await createDatabaseIfNotExists()
     await connectDB()
+    modelRelations()
+
+    setInterval(() => {
+        ReservaService.updateExpiredReservations();
+    }, 60000);
+
     const server = Server.getInstance()
     server.listen()
 }
