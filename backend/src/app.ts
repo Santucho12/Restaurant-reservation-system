8b7909ace1fb7414cf891ff89f086829fe4f5502
NACXIIX <nicocordano@hotmail.com>
2025-11-27 16:34:08 -0300

    feat(backend): conexion data base implementada- falta refactorizar singleton

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,9 @@
 import Server from './config/appConfig'
+import { connectDB, createDatabaseIfNotExists } from './config/dbConfig'
 
 async function app(): Promise<void> {
+    await createDatabaseIfNotExists()
+    await connectDB()
     const server = Server.getInstance()
     server.listen()
 }
--- backend/src/config/dbConfig.ts
+++ backend/src/config/dbConfig.ts
@@ -23,4 +23,29 @@ const sequelize = new Sequelize(
     }
 )
 
+export const connectDB = async () => {
+    try {
+        await sequelize.authenticate()
+        console.log("Conexion exitosa a la base de datos")
+
+        await sequelize.sync({ force: false })
+        console.log('Base de datos y tablas creadas correctamente.')
+    } catch (error) {
+        console.error('Error conectando a la base de datos:', error)
+    }
+};
+
+export const createDatabaseIfNotExists = async () => {
+    const connection = await mysql.createConnection({
+        host: process.env.DB_HOST || 'localhost',
+        user: process.env.DB_USER || 'userrrs',
+        password: process.env.DB_PASSWORD || 'metodologia2',
+        port: Number(process.env.DB_PORT) || 3306,
+    });
+
+    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${process.env.DB_NAME}\`;`);
+    console.log(`📦 Base de datos '${process.env.DB_NAME}' verificada o creada`);
+    await connection.end();
+};
+
 export default sequelize;
