e3e6c8965d241366eab94dbb8abf1e34a6d5286b
NACXIIX <nicocordano@hotmail.com>
2025-11-27 15:52:28 -0300

    feat(backend): creacion de las rutas de Mesa

---
+++ backend/src/routes/mesaRoutes.ts
@@ -0,0 +1,13 @@
+// Endpoints para crear, ver y cancelar mesas
+import Router from 'express'
+import MesaController from '../controllers/MesaController'
+
+const mesaRoutes = Router()
+
+mesaRoutes.get('/mesas', MesaController.getAllMesas)
+mesaRoutes.get('/mesas/:id', MesaController.getMesa)
+mesaRoutes.post('/mesas', MesaController.createMesa)
+mesaRoutes.put('/mesas/:id', MesaController.updateMesa)
+mesaRoutes.delete('/mesas/:id', MesaController.deleteMesa)
+
+export default mesaRoutes
