38058fcd894f567bc015501d70fe454daca723b4
NACXIIX <nicocordano@hotmail.com>
2025-11-27 17:38:54 -0300

    feat(backend): implementado el servicio usando Op de sequelize para controlar las fechas

--- backend/src/services/ReservaService.ts
+++ backend/src/services/ReservaService.ts
@@ -1,1 +1,20 @@
-// Lógica principal y validaciones
+import Reserva from '../models/Reserva'
+import { Op } from 'sequelize'
+
+export default new class ReservaService {
+    async updateExpiredReservations() {
+        try {
+            const now = new Date();
+            await Reserva.update({ estado: 'finalizada' },
+                {
+                    where: {
+                        fechaFin: { [Op.lt]: now },
+                        estado: { [Op.notIn]: ['finalizada', 'cancelada'] }
+                    }
+                }
+            )
+        } catch (error) {
+            console.error('Error actualizando reservas expiradas:', error);
+        }
+    }
+}
