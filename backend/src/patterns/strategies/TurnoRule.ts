e48f611362454fd2d983531208adff26cd196888
NACXIIX <nicocordano@hotmail.com>
2026-01-25 18:18:42 -0300

    feat(backend): regla para validar que sea correcto el horario de un turno de una reserva

---
+++ backend/src/patterns/strategies/TurnoRule.ts
@@ -0,0 +1,18 @@
+import { ValidationRule, ValidationData } from './ValidacionRule';
+
+export class TurnoRule implements ValidationRule {
+  async validar(datos: ValidationData): Promise<void> {
+    const { 'fecha/hora': fechaInicio } = datos;
+    const fecha = new Date(fechaInicio);
+    const hora = fecha.getHours();
+
+    const esAlmuerzo = hora >= 12 && hora < 15;
+    const esCena = hora >= 20 && hora < 23;
+
+    if (!esAlmuerzo && !esCena) {
+      throw new Error(
+        'La reserva debe ser en horario de almuerzo (12-15) o cena (20-23)',
+      );
+    }
+  }
+}
