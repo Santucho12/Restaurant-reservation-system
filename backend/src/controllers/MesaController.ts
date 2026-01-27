307cf57bf3fe917fce21a29bc5cada106ddeb545
Santucho12 <santysegal@gmail.com>
2026-01-26 22:42:51 -0300

    feat(backend): Manejador de errores en controlador de mesa creado

--- backend/src/controllers/MesaController.ts
+++ backend/src/controllers/MesaController.ts
@@ -1,17 +1,18 @@
 // Lógica de mesas
 import { Request, Response } from 'express';
 import Mesa from '../models/Mesa';
+import ErrorHandler from '../utils/ErrorHandler';
 
 export default new (class MesaController {
   async getAllMesas(req: Request, res: Response) {
     try {
       const mesa = await Mesa.findAll();
       if (!mesa) {
-        return res.status(404).json({ message: 'No hay mesas' });
+        return ErrorHandler.notFoundErrorMesa(res);
       }
       res.status(200).json(mesa);
     } catch (error) {
-      res.status(500).json({ error: error });
+      ErrorHandler.serverInternalError(res, error as Error);
     }
   }
 
@@ -22,12 +23,12 @@ export default new (class MesaController {
       const mesa = await Mesa.findByPk(idNumber);
 
       if (!mesa) {
-        return res.status(404).json({ message: 'No hay mesas' });
+        return ErrorHandler.notFoundErrorMesa(res);
       }
 
       res.status(200).json(mesa);
     } catch (error) {
-      res.status(500).json({ error: error });
+      ErrorHandler.serverInternalError(res, error as Error);
     }
   }
 
@@ -36,21 +37,21 @@ export default new (class MesaController {
       const newMesa = await Mesa.create(req.body);
       res.status(201).json(newMesa);
     } catch (error) {
-      res.status(500).json({ error: error });
+      ErrorHandler.serverInternalError(res, error as Error);
     }
   }
 
   async updateMesa(req: Request, res: Response) {
     try {
       const id = req.params.id;
-      const updateMesa = Mesa.update(req.body, { where: { id } });
+      const updateMesa = await Mesa.update(req.body, { where: { id } });
       if (!updateMesa) {
-        return res.status(404).json({ message: 'No hay mesas' });
+        return ErrorHandler.notFoundErrorMesa(res);
       }
 
       res.status(200).json({ message: 'Mesa actualizada' });
     } catch (error) {
-      res.status(500).json({ error: error });
+      ErrorHandler.serverInternalError(res, error as Error);
     }
   }
 
@@ -60,15 +61,13 @@ export default new (class MesaController {
       const idNumber = Number(id);
       const mesa = await Mesa.findByPk(idNumber);
       if (!mesa) {
-        return res.status(404).json({ message: 'No hay mesas' });
+        return ErrorHandler.notFoundErrorMesa(res);
       }
 
-      mesa.destroy();
-      return res
-        .status(200)
-        .json({ message: 'Mesa eliminada satisfactoriamente' });
+      await mesa.destroy();
+      return res.status(200).json({ message: 'Mesa eliminada satisfactoriamente' });
     } catch (error) {
-      res.status(500).json({ error: error });
+      ErrorHandler.serverInternalError(res, error as Error);
     }
   }
 })();
