267b95f4969bb6bb5e99f932abea6a5aeedf3c35
NACXIIX <nicocordano@hotmail.com>
2026-01-31 23:10:29 -0300

    feat(backend): administrador predeterminado

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,12 +2,14 @@ import Server from './config/appConfig';
 import Database, { createDatabaseIfNotExists } from './config/dbConfig';
 import { modelRelations } from './models/ModelsRelations';
 import ReservaService from './services/ReservaService';
+import { createDefaultAdmin } from './utils/initAdmin';
 
 async function app(): Promise<void> {
   await createDatabaseIfNotExists();
   const db = Database.getInstance();
   await db.connect();
   modelRelations();
+  await createDefaultAdmin();
 
   setInterval(() => {
     ReservaService.updateExpiredReservations();
